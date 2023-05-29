c360a47ad2399ffb8bae3193c124e6d90ab9d0c8
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2023-05-29 15:32:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function volume_sphere() {
     //Write your code here
+	let radius=parseFloat(document.getElementById("radius").value);
+	var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+	document.getElementById("volume").value = volume;
   
 } 
 
